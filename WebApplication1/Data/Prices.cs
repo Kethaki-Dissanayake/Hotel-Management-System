cdf239aef29e167c3fa8452dd5d99301081d1849
Kethaki Dissanayake <60033672+Kethaki-Dissanayake@users.noreply.github.com>
2022-08-25 23:33:26 +0530

    Prices

---
+++ WebApplication1/Data/Prices.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Data
+{
+    public class Prices
+    {
+        [Key]
+        public int PriceId { get; set; }
+        public string Code { get; set; }
+        public string CurrentPrice { get; set; }
+        
+    }
+}
